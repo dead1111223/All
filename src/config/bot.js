62389fd2590ad5eb2bd162c38493a36b34dc2033
dead1111223 <jepeseleg@gmail.com>
2026-07-20 20:54:19 +0200

    Change bot activity name from 'roblox' to 'ntm suak'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "roblox",
+        name: "ntm suak",
         // Activity type number (0 = Playing).
         type: 0, 
       },
